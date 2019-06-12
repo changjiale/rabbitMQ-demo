ca60b91fbf72f10152656f2dbc418dc4106e0c40
changjiale <1056430312@qq.com>
2019-06-12 15:02:20 +0800

    rabbitmq简单模式

---
+++ src/main/java/cn/itcast/rabbitmq/spring/Application.java
@@ -0,0 +1,16 @@
+package cn.itcast.rabbitmq.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: HuYi.Zhang
+ * @create: 2018-05-23 18:07
+ **/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
